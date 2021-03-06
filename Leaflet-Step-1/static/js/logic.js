d3459f1e6293ff0d410c3aadf9bd8d96f7002dcb
perkins228 <perkins.228@gmail.com>
2021-03-05 19:48:08 -0500

    map layer

---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,21 @@
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+d3.json(queryUrl, function(data) {
+    console.log(data)
+})
+
+var myMap = L.map("map", {
+    center: [39.739, -104.99],
+    zoom: 3
+  });
+  
+  // Adding a tile layer (the background map image) to our map
+  // We use the addTo method to add objects to our map
+  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  }).addTo(myMap);
+  
